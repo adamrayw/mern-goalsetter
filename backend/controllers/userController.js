037de6e65f1e0d43df7c21c0676cd1ecf6106055
adamrayw <raywibowo68@gmail.com>
2022-07-29 20:18:39 +0700

    add two method login and get user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,22 @@
+// @desc    Register Users
+// @route   POST /api/users/register
+// @access  Public
 const registerUser = (req, res) => {
     res.json({ message: 'Register User' })
 }
 
-module.exports = { registerUser }
+// @desc    Authenticate a User
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = (req, res) => {
+    res.json({ message: 'Login User' })
+}
+
+// @desc    Get a user data
+// @route   POST /api/user/me
+// @access  Private
+const getMe = (req, res) => {
+    res.json({ message: 'User data display' })
+}
+
+module.exports = { registerUser, loginUser, getMe }
