abad4f10a5450cc3975bc26ce95e82d2e9623cf2
adamrayw <raywibowo68@gmail.com>
2022-07-29 19:19:52 +0700

    add userRoutes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 const connectDB = require('./config/db')
-const goalRoute = require('./routes/goalRoutes')
+const goalRoutes = require('./routes/goalRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 connectDB()
 
@@ -13,7 +14,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/goals', goalRoute)
+app.use('/api/goals', goalRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(errorHandler)
 
