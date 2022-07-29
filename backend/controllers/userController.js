63555a7cbcd62380ff30053520db3c95230cc6f6
adamrayw <raywibowo68@gmail.com>
2022-07-29 23:36:07 +0700

    add token object key, login feature have been completed, add generateToken method to generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,8 @@ const registerUser = async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -52,14 +53,42 @@ const registerUser = async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = async (req, res) => {
-    res.json({ message: 'Login User' })
+    const { email, password } = req.body
+
+    // Check for user email
+    const user = await User.findOne({ email })
+
+    if (user && (bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 }
 
 // @desc    Get a user data
 // @route   POST /api/user/me
 // @access  Private
 const getMe = async (req, res) => {
-    res.json({ message: 'User data display' })
+    const { _id, name, email } = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
+}
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
 }
 
 module.exports = { registerUser, loginUser, getMe }
