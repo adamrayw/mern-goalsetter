1df1900a3850f489eb775ac9842f0868c3b7ab18
adamrayw <raywibowo68@gmail.com>
2022-08-01 21:11:14 +0700

    add register

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { useSelector, useDispatch } from 'react-redux'
+import authService from './authService'
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
@@ -11,6 +13,17 @@ const initialState = {
     message: ''
 }
 
+// Register User
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+    try {
+        return await authService.register(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toSttring()
+
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
@@ -22,7 +35,23 @@ export const authSlice = createSlice({
             state.message = ''
         }
     },
-    extraReducers: () => { }
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.pending, (state, action) => {
+                state.isLoading = true
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
+    }
 })
 
 
