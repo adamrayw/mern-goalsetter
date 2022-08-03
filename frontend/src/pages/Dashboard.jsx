b4d1a177165f31271165e750826d70b3427aeeb2
adamrayw <raywibowo68@gmail.com>
2022-08-03 22:20:30 +0700

    restrict dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,24 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 function Dashboard() {
+    const navigate = useNavigate()
+
+    const { user } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login')
+        }
+    }, [user, navigate])
+
     return (
-        <>Dashboard</>
+        <>
+            <section className="heading">
+                <h1>Welcome {user && user.name}</h1>
+            </section>
+        </>
     )
 }
 
