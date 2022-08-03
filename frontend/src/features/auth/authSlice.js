044e9755d78f92fffadbd2815297b750e2f2c4e9
adamrayw <raywibowo68@gmail.com>
2022-08-03 23:48:12 +0700

    fix typo

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -17,7 +17,7 @@ export const register = createAsyncThunk('auth/register', async (user, thunkAPI)
     try {
         return await authService.register(user)
     } catch (error) {
-        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toSttring()
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
 
         return thunkAPI.rejectWithValue(message)
     }
@@ -28,7 +28,7 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
     try {
         return await authService.login(user)
     } catch (error) {
-        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toSttring()
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
 
         return thunkAPI.rejectWithValue(message)
     }
